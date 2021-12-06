4189bc80c4bf9afd52df67d078ee985e2ba04144
Eloísa Bazzanella <elobazzanella@gmail.com>
2021-12-06 20:05:47 -0300

    Add .env
    Adicionado .env

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,22 @@
 import { createConnection } from "typeorm";
 
-createConnection();
+console.log(process.env);
+
+createConnection({
+    type: "postgres",
+    host: String(process.env.DB_HOST),
+    port: Number(process.env.DB_PORT),
+    username: String(process.env.DB_USERNAME),
+    password: String(process.env.DB_PASSWORD),
+    database: String(process.env.DB_NAME),
+    migrations: [
+        "src/database/migrations/*.ts"
+    ],
+    entities: [
+        "src/entities/*.ts"
+    ],
+    cli: {
+        migrationsDir: "src/database/migrations",
+        entitiesDir: "src/entities"
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import { config } from "dotenv";
+
+config();
 
 import { router } from "./routes";
 
