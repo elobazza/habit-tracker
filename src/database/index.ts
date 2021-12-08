ac3110c25f246f6b19bdea4e6d04221b7afc0110
Eloísa Bazzanella <elobazzanella@gmail.com>
2021-12-07 22:38:28 -0300

    Adjusted create connection config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,18 @@
 import { createConnection } from "typeorm";
 
-createConnection();
+createConnection({
+    name: "default",
+    type: "postgres",
+    host: String(process.env.TYPEORM_HOST),
+    port: Number(process.env.TYPEORM_PORT),
+    username: String(process.env.TYPEORM_USERNAME),
+    password: String(process.env.TYPEORM_PASSWORD),
+    database: String(process.env.TYPEORM_DATABASE),
+    entities: [
+        String(process.env.TYPEORM_ENTITIES),
+    ],
+    migrations: [
+        String(process.env.TYPEORM_MIGRATIONS),
+    ],
+    migrationsRun: true,
+});
