ec6e7bd3eaf62022bcc6fc9dce055805a6e0f8ff
Eloísa Bazzanella <elobazzanella@gmail.com>
2021-12-09 22:28:17 -0300

    Ajuste

--- src/services/ShowUserService.ts
+++ src/services/ShowUserService.ts
@@ -5,7 +5,7 @@ class ShowUserService {
     async execute(idusuario: string) {
         const userRepositories = getCustomRepository(UserRepositories);
 
-        const user = await userRepositories.find({
+        const user = await userRepositories.findOne({
             idusuario
         });
 
